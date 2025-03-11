69901d4c33d1979a890f1fe79359972c9eeecb68
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-03-11 21:15:41 +0000

    Update wind-study.py

--- wind-study.py
+++ wind-study.py
@@ -58,7 +58,7 @@ fig = go.Figure(data=go.Contour(
         showlines=True,
         start=df['Z'].min(),
         end=df['Z'].max(),
-        size=0.05  # Adjust the contour interval as needed.
+        size=0.1  # Adjust the contour interval as needed.
     ),
     colorbar=dict(title="Contour Level")
 ))
