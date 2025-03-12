ba14be4a97dbeb80271fdd632da2a7d3f153a3a6
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-03-12 11:58:02 +0000

    Update wind-study.py

--- wind-study.py
+++ wind-study.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.interpolate import griddata
 
 # Set page configuration
-st.set_page_config(page_title="Contour Analysis Tool", layout="wide")
+st.set_page_config(page_title="Contour Analysis Tool")
 
 # -----------------------
 # Authentication Section
@@ -140,7 +140,7 @@ def create_contour_plot(df, sheet_name, x_input, y_input):
                 x=0.5,
                 y=0.5
             )],
-            height=400,
+            height=500,
             margin=dict(l=50, r=50, t=50, b=50)
         )
         return fig, None
@@ -231,7 +231,7 @@ def create_contour_plot(df, sheet_name, x_input, y_input):
             gridcolor='rgba(200,200,200,0.2)',
             range=[np.log10(y_min), np.log10(y_max)]
         ),
-        height=400,
+        height=500,
         plot_bgcolor='rgba(240,240,240,0.95)',
         margin=dict(l=50, r=50, t=50, b=50)
     )
